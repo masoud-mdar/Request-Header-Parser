2ad8e10fd7b6f629ffd39ebf1b83c9e44a788acd
Masoud <masoud_mdar@yahoo.fr>
2021-05-09 20:07:38 +0200

    loading page

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,46 @@ const BASE_URL = "https://req-header-prsr.herokuapp.com"
 
 const App = () => {
     const [result, setResult] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
-    const handleClick = (data) => {
+    const handleClick = () => {
+        setIsLoading(true)
         axios.get(`${BASE_URL}/whoami`).then((response) => {
             setResult(response.data)
-            console.log(response.data)
-            console.log(result)
+            setIsLoading(false)
         })
     }
 
     return(
-        <div className="container">
-            <button className="btn" onClick={handleClick}>Who am I ?</button>
-            {result.ipaddress ? (
-                <div className="info">
-                    <div>
-                    <p>IP Adress:</p><h2> {result.ipaddress}</h2>
-                    </div>
-                    <div>
-                    <p>Language:</p><h2> {result.language}</h2>
-                    </div>
-                    <div>
-                    <p>Software:</p><h2> {result.software}</h2>
-                    </div>
-                    
-                </div>
-            ) : (
-                <div></div>
-            )}
+        <div>
+            {!isLoading ? (
 
+                <div className="container">
+                    <button className="btn" onClick={handleClick}>Who am I ?</button>
+                    {result.ipaddress ? (
+                        <div className="info">
+                            <div>
+                            <p>IP Adress:</p><h2> {result.ipaddress}</h2>
+                            </div>
+                            <div>
+                            <p>Language:</p><h2> {result.language}</h2>
+                            </div>
+                            <div>
+                            <p>Software:</p><h2> {result.software}</h2>
+                            </div>
+                            
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
+
+                </div>
+
+            ) : (
+                <div className="loading"><h1>Loading...</h1></div>
+            )}
         </div>
+
     )
 }
 
